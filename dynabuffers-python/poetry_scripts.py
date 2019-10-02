fcf32feafbf639a6aa4cb44c0fbd6180a2b233f5
fridayy <benjamin.krenn@leftshift.one>
2019-10-02 11:11:01 +0200

    release capabilities and ci added

---
+++ dynabuffers-python/poetry_scripts.py
@@ -0,0 +1,26 @@
+from subprocess import check_call
+import sys
+import re
+
+def test_ci():
+    check_call(["poetry", "run", "pytest", '--junit-xml=$TEST_RESULTS_PATH/TEST-@filename@-junit.xml'])
+
+def release():
+    tag = sys.argv[1]
+    print("Releasing version", tag)
+    version = _version_from_tag(tag)
+    check_call(["poetry", "run", "test_ci"])
+    check_call(["poetry", "build"])
+    check_call(["poetry", "version", version])
+    check_call(["poetry", "publish", "-u", "$PYPI_USERNAME", "-p", "$PYPI_PASSWORD"])
+
+def _version_from_tag(tag):
+    """
+    Extracts the semver from the given tag (e.g: v0.1.0 -> 0.1.0)
+    :param tag:
+    :return:
+    """
+    pattern = re.compile('v\d+.\d+.\d+')
+    if not pattern.match(tag):
+        raise Exception(f"{tag} is not valid semver")
+    return tag[1:]
