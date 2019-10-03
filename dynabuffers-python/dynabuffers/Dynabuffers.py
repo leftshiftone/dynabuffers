8a0810c8f3fee27e3ea7079b9a0efd726d1f586c
christian <christian.weber@leftshift.one>
2019-10-03 13:42:33 +0200

    Dynabuffers.py modified

--- dynabuffers-python/dynabuffers/Dynabuffers.py
+++ dynabuffers-python/dynabuffers/Dynabuffers.py
@@ -8,8 +8,8 @@ from dynabuffers.antlr.DynabuffersParser import DynabuffersParser
 
 class Dynabuffers:
     @staticmethod
-    def parse(schema: InputStream) -> DynabuffersEngine:
-        lexer = DynabuffersLexer(schema)
+    def parse(schema) -> DynabuffersEngine:
+        lexer = DynabuffersLexer(Dynabuffers.stream(schema))
         stream = CommonTokenStream(lexer)
         parser = DynabuffersParser(stream)
 
@@ -17,3 +17,15 @@ class Dynabuffers:
         tree = visitor.visit(parser.compilation())
 
         return DynabuffersEngine(tree)
+
+    @staticmethod
+    def stream(obj):
+        if isinstance(obj, InputStream):
+            return obj
+        if isinstance(obj, str):
+            return InputStream(str(obj))
+        if isinstance(obj, bytes):
+            return InputStream(obj.decode("utf-8"))
+        if isinstance(obj, bytearray):
+            return InputStream(obj.decode("utf-8"))
+        raise ValueError(str(obj) + " cannot be converted to input stream")
