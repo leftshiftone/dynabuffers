455ad525751f0f17453a563e8ae75e2b4af4b376
Raphael Schluesselbauer <raphael.schluesselbauer@leftshift.one>
2021-03-24 10:26:20 +0100

    [GAIA-2950] Fix default values for int, long and short

---
+++ dynabuffers-python/tests/usecase/Schema26Test.py
@@ -0,0 +1,29 @@
+import os
+import unittest
+
+from antlr4 import FileStream
+
+from dynabuffers.Dynabuffers import Dynabuffers
+
+
+class Schema26(unittest.TestCase):
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+
+    def setUp(self):
+        self.engine = Dynabuffers.parse(FileStream(self.root_dir + "/schema26.dbs"))
+
+    def test_defaults(self):
+        obj = {
+            "text": "123",
+            "oneInt": 1,
+            "twoFloat": 2.0,
+            "threeShort": 3,
+            "fourLong": 4,
+            "trueBoolean": True
+        }
+        result = self.engine.deserialize(self.engine.serialize({}))
+        self.assertEqual(result, obj)
+
+
+if __name__ == "__main__":
+    unittest.main()
